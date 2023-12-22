0a4938f48aedd66aeff6ec84d34581845009b129
Anak-caetano <anak.94c@gmail.com>
2023-12-22 13:06:10 -0300

    Nome e tipo de projeto no form de seleção

--- script.js
+++ script.js
@@ -2,38 +2,53 @@ let tipoProjeto;
 
 function votacao() {
     tipoProjeto = document.getElementById("tipoProjeto").value;
-
+    
     switch (tipoProjeto) {
         case "fotografia":
-        case "composicao":
-        case "colagem":
-        case "interface":
-            alert(`O Projeto selecionado foi ${tipoProjeto}`);
+            case "composicao":
+                case "colagem":
+                    case "interface":
+                        alert(`O Projeto selecionado foi ${tipoProjeto}`);
+                        break;
+                        case "":
+                            alert("Valor inválido");
+                            break;
+                            default:
+                                alert("Tipo de projeto não reconhecido");
             break;
-        case "":
-            alert("Valor inválido");
-            break;
-        default:
-            alert("Tipo de projeto não reconhecido");
-            break;
+        }
+        
+        // Impedir o envio padrão do formulário
+        return true;
     }
 
-    // Impedir o envio padrão do formulário
-    return true;
-}
 
-function transferirDados() {
-    let nomeAluno = document.getElementsByName('nome')[0].value;
+// Capturar nome e tipo de projeto do aluno avaliado
+const submitForm = document.querySelector('.login-form');
+const spanNome = document.getElementById('nomeResultado');
+const spanTipoProjeto = document.getElementById('tipoProjetoSelecionado');
 
-    sessionStorage.setItem('nomeAluno', nomeAluno);
-    sessionStorage.setItem('tipoProjeto', tipoProjeto);
-}
+function transferirDados(event) {
+    event.preventDefault();
+    
+    const nomeAluno = document.getElementById("nome").value;
+    const tipoProjetoSelecionado = document.getElementById("tipoProjeto").value;
 
-document.addEventListener('DOMContentLoaded', function () {
-    let nomeAluno = sessionStorage.getItem('nomeAluno');
-    let resultadoTipoProjeto = sessionStorage.getItem('tipoProjeto');
+    localStorage.setItem('nomeAluno', nomeAluno);
+    localStorage.setItem('tipoProjeto', tipoProjetoSelecionado);
+    window.location = "apurar.html";
+};
 
-    // Exibir os valores na segunda página
-    document.getElementById('nomeResultado').innerText = nomeAluno;
-    document.getElementById('tipoProjetoResultado').innerText = resultadoTipoProjeto;
+// submitForm.addEventListener('submit', transferirDados);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nomeArmazenado = localStorage.getItem('nomeAluno');
+    const tipoProjetoArmazenado = localStorage.getItem('tipoProjeto');
+    alert(`Estudante ${nomeArmazenado} selecionou o projeto ${tipoProjetoArmazenado}`);
+
+    // Verificar se os valores existem antes de exibi-los
+    if (nomeArmazenado && tipoProjetoArmazenado) {
+        spanNome.innerText = nomeArmazenado;
+        spanTipoProjeto.innerText = tipoProjetoArmazenado;
+    }
 });
